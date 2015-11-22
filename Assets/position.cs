96dbfc81d7775777365da1e83e988a2d6f48aa9c
daniel <daniel@foolheadstudio.com>
2015-11-22 13:41:22 +0100

    E01_CASA MASTER

---
+++ Assets/position.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class position : MonoBehaviour {
+	public GameObject parent;
+	// Use this for initialization
+	void Start () {
+		this.transform.position = parent.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.position = parent.transform.position;
+	}
+}
+
